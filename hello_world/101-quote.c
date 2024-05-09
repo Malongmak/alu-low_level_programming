c3bdadae219661f4314998d366fed2a046d46b93
Malongmak <j.malong@alustudent.com>
2024-05-09 02:50:44 -0700

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
- * Return: Always 1 (Success)
+ *
+ * Return: Always 1
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+    char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, quote, sizeof(quote) - 1);
+    return (1);
 }

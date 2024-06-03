9d73d82526aee3802deafa8a8504de73b9679341
Malongmak <j.malong@alustudent.com>
2024-06-03 03:19:15 -0700

    1-swap.c

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -2,10 +2,10 @@
 
 /**
  * Swap_int - Swaps the values of two integers.
- * @a: The first integer to be swapped
- * @b: The second integer to be swapped
+ * @a: The first integer to be swapped.
+ * @b: The second integer to be swapped.
  *
- * Return: nothing
+ * Return: nothing.
  */
 void swap_int(int *a, int *b)
 /* the function that swaps the values of two integers. */

a3ab63b855346ced262e2263937555baf107e6e6
Malongmak <j.malong@alustudent.com>
2024-07-07 04:27:23 -0700

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,16 +1,22 @@
 #include "function_pointers.h"
-	#include <stdio.h>
-	/**
-	 * print_name - print name using pointer to function
-	 * @name: string to add
-	 * @f: pointer to function
-	 * Return: nothing
-	 **/
-	void print_name(char *name, void (*f)(char *))
-	{
-		if (name == NULL || f == NULL)
-			return;
+#include <stdio.h>
 
+/**
+ * print_name - Prints a name using a pointer to function
+ * @name: The name string to print
+ * @f: Pointer to a function that takes a char * argument
+ *     and returns void
+ *
+ * Description: This function takes a name string and a pointer
+ *              to a function. If `name` and `f` are not NULL,
+ *              it calls the function `f` with the `name` as
+ *              an argument.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
 
-		f(name);
-	}
+	f(name);
+}
+

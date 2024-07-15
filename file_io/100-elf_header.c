d35a26517e87f4c7b6ff47fdb34ca1edcc3b3564
Malongmak <j.malong@alustudent.com>
2024-07-15 03:45:17 -0700

    100-elf_header.c

--- file_io/100-elf_header.c
+++ file_io/100-elf_header.c
@@ -1,10 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
-#include <errno.h>
-#include <elf.h>
 
 /**
  * elf_header - displays the information contained in the ELF header
